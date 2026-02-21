a053755149dcf18d2be3d767d7dc67e3f117d84e
vinithvrangan <vinithrangan@gmail.com>
2026-02-21 12:29:42 +0530

    profile testing 6

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 const MKAVS_CONFIG = {
     // API_BASE_URL: 'http://localhost:3000' // Local development
     API_BASE_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-        ? 'http://localhost:3000'
+        ? 'http://dev.mkavs.com'
         : 'https://mkavs-backend.onrender.com'
 };
