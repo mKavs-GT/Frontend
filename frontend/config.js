8abcfb94d7b664f9d774fe1cbbefbe46dea464e3
vinithvrangan <vinithrangan@gmail.com>
2026-03-09 22:08:12 +0530

    config.js is updated

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,7 @@
 const MKAVS_CONFIG = {
-    // API_BASE_URL: 'http://localhost:3000' // Local development
-    API_BASE_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-        ? 'http://dev.mkavs.com'
-        : 'https://mkavs-backend.onrender.com'
+    API_BASE_URL:
+        window.location.hostname === 'localhost' ||
+            window.location.hostname === '127.0.0.1'
+            ? 'http://localhost:3000'
+            : 'https://api.mkavs.com'
 };
