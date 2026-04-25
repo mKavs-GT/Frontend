6004474b7de0e6a82267af8906446a4fb94c68e3
vinithvrangan <vinithrangan@gmail.com>
2026-04-25 13:52:25 +0530

    admin panel 2

--- frontend/admin/vite.config.js
+++ frontend/admin/vite.config.js
@@ -43,4 +43,13 @@ export default defineConfig({
       }
     })
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
