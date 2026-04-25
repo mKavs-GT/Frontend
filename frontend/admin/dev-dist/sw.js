4dae45f0534d27fe32f6b5fa167ab5b20129e619
vinithvrangan <vinithrangan@gmail.com>
2026-04-25 11:38:54 +0530

    bringing admin panel back

--- frontend/admin/dev-dist/sw.js
+++ frontend/admin/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ibf0b2e689s"
+    "revision": "0.srhrnajqftc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
