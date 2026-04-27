2038feacc0fa59b1a83d299893a7169a65254995
vinithvrangan <vinithrangan@gmail.com>
2026-04-27 21:11:10 +0530

    admin panel changes 1

--- frontend/admin/src/components/Login.jsx
+++ frontend/admin/src/components/Login.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react';
 import { motion } from 'framer-motion';
 
 const allowedUsers = [
-  { uid: 'MGT-EXE-01', email: 'agent01mrk@gmail.com', name: 'Krishawn Rahul', firstName: 'Krishawn', avatar: 'https://i.pravatar.cc/150?u=krishawn', role: 'Executive', isExecutive: true },
-  { uid: 'MGT-DEV-01', email: 'agent03mrss@gmail.com', name: 'Sofia Stalance', firstName: 'Sofia', avatar: 'https://i.pravatar.cc/150?u=sofia', role: 'Developer' },
-  { uid: 'MGT-DEV-02', email: 'agent05mrm@gmail.com', name: 'Michael Antony', firstName: 'Michael', avatar: 'https://i.pravatar.cc/150?u=michael', role: 'Developer' },
-  { uid: 'MGT-DES-01', email: 'agent04mra@gmail.com', name: 'Mohammed Abuzar', firstName: 'Mohammed', avatar: 'https://i.pravatar.cc/150?u=mohammed', role: 'Designer' },
-  { uid: 'MGT-EXE-02', email: 'agent02mrv@gmail.com', name: 'Vinith Vijaya Rangan', firstName: 'Vinith', avatar: 'https://i.pravatar.cc/150?u=vinith', role: 'Executive' },
-  { uid: 'MGT-BIZ-01', email: 'agent06mrz@gmail.com', name: 'Sitesh', firstName: 'Sitesh', avatar: 'https://i.pravatar.cc/150?u=sitesh', role: 'Business' }
+  { uid: 'MGT-EXE-01', email: 'agent01mrk@gmail.com', name: 'Mr.K', firstName: 'Mr.K', avatar: 'https://i.pravatar.cc/150?u=krishawn', role: 'Executive', isExecutive: true },
+  { uid: 'MGT-DEV-01', email: 'agent03mrss@gmail.com', name: 'Mrs.S', firstName: 'Mrs.S', avatar: 'https://i.pravatar.cc/150?u=sofia', role: 'Developer' },
+  { uid: 'MGT-DEV-02', email: 'agent05mrm@gmail.com', name: 'Mr.M', firstName: 'Mr.M', avatar: 'https://i.pravatar.cc/150?u=michael', role: 'Developer' },
+  { uid: 'MGT-DES-01', email: 'agent04mra@gmail.com', name: 'Mr.A', firstName: 'Mr.A', avatar: 'https://i.pravatar.cc/150?u=mohammed', role: 'Designer' },
+  { uid: 'MGT-EXE-02', email: 'agent02mrv@gmail.com', name: 'Mr.V', firstName: 'Mr.V', avatar: 'https://i.pravatar.cc/150?u=vinith', role: 'Executive' },
+  { uid: 'MGT-BIZ-01', email: 'agent06mrz@gmail.com', name: 'Mr.Z', firstName: 'Mr.Z', avatar: 'https://i.pravatar.cc/150?u=sitesh', role: 'Business' }
 ];
 
 export default function Login({ onLogin }) {
