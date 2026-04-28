d8e45d387f417b8f87b992ffc8ef6f7ec5ed6d41
0xm1chael <michael.araj006@gmail.com>
2026-04-29 01:30:24 +0530

    Kairon Bug Fixes 2

--- frontend/admin/dev-dist/sw.js
+++ frontend/admin/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tdjl9uhin28"
+    "revision": "0.qrdocm7908s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
