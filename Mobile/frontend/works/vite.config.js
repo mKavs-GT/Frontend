c3275582707a87997a1a4a3ada389c8f09d6c372
Sofia-Stalance <sofiastalance@gmail.com>
2026-06-28 18:45:25 +0530

    portfolio update

--- Mobile/frontend/works/vite.config.js
+++ Mobile/frontend/works/vite.config.js
@@ -6,4 +6,12 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: './',
+  server: {
+    proxy: {
+      '/Portfolio': {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
+      }
+    }
+  }
 })
